14365e05b0d404492e71de3f5b44b644493c837f
Hyxpillow <401887202@qq.com>
2020-11-23 23:16:55 +0800

    debug snake and bird

--- Flappy_Bird.cpp
+++ Flappy_Bird.cpp
@@ -2,53 +2,53 @@
 #include "Flappy_Bird.h"
 
 
-int Map[100][100];
-int Map_height;
-int Map_width;
+int bird_Map[100][100];
+int bird_Map_height;
+int bird_Map_width;
 int bird_x;
 int bird_y;
 int tube_x;
 int tube_y;
-int Score;
-
+int bird_Score;
+int bird_flag;
 
 
 void InitData() {
-	Map_height = 30;
-	Map_width = 50;
-	bird_x = Map_width / 4;
-	bird_y = Map_height / 2;
-	tube_x = Map_width / 4 * 3;
-	tube_y = Map_height / 2;
-	Score = 0;
+	bird_Map_height = 20;
+	bird_Map_width = 40;
+	bird_x = bird_Map_width / 4;
+	bird_y = bird_Map_height / 2;
+	tube_x = bird_Map_width / 4 * 3;
+	tube_y = bird_Map_height / 2;
+	bird_Score = 0;
+	bird_flag = 0;
 	HideCursor();
 	//解释：设置地图幅度，鸟的坐标，水管（障碍）的坐标
 }
 void InitMap() {
-	system("cls");
-	for (int i = 1; i <= Map_height - 1; i++) {
-		Map[i][1] = 4;
-		for (int j = 2; j < Map_width - 1; j++)
-			Map[i][j] = 0;
-		Map[i][Map_width] = 4;
+	for (int i = 1; i <= bird_Map_height - 1; i++) {
+		bird_Map[i][1] = 4;
+		for (int j = 2; j < bird_Map_width - 1; j++)
+			bird_Map[i][j] = 0;
+		bird_Map[i][bird_Map_width] = 4;
 	}
 	for (int j = 1; j < tube_y; j++) {
-		Map[j][tube_x] = 2;
+		bird_Map[j][tube_x] = 2;
 	}
-	for (int j = tube_y + 2; j < Map_height; j++) {
-		Map[j][tube_x] = 2;
+	for (int j = tube_y + 4; j < bird_Map_height; j++) {
+		bird_Map[j][tube_x] = 2;
 	}
-	for (int j = 1; j <= Map_width; j++) {
-		Map[Map_height][j] = 3;
+	for (int j = 1; j <= bird_Map_width; j++) {
+		bird_Map[bird_Map_height][j] = 3;
 	}
-	Map[bird_x][bird_y] = 1;
+	bird_Map[bird_x][bird_y] = 1;
 }
 //解释：由于最终的界面是和一个二维数组完全对应。所以初始化数组就显得非常重要。这里将围墙，水管，小鸟对应的二维数组上的位置都设定了不同的值，便于对应画图。
 void draw() {
-	gotoxy(0, 0);
-	for (int i = 1; i <= Map_height; i++) {
-		for (int j = 1; j <= Map_width; j++) {
-			switch (Map[i][j]) {
+	system("cls");
+	for (int i = 1; i <= bird_Map_height; i++) {
+		for (int j = 1; j <= bird_Map_width; j++) {
+			switch (bird_Map[i][j]) {
 			case 0:
 				printf(" ");
 				break;
@@ -69,7 +69,7 @@ void draw() {
 		printf("\n");
 	}
 	printf("Flappy Bird\n");
-	printf("您当前的分数是%d", Score);
+	printf("您当前的分数是%d", bird_Score);
 }
 //解释：gotoxy找到起始位置，然后对应数组上的值来画图。并有一行文字显示得分
 bool Update() {
@@ -77,50 +77,83 @@ bool Update() {
 	if (_kbhit()) {
 		//解释：_kbhit()只要有键盘敲击，就为真，然后同时为_getch输入
 		input = _getch();
-		if (input == ' ');
-		bird_x = bird_x - 1;
+		//if (input == ' ');
+		gotoxy(bird_y, bird_x);
+		putchar(' ');
+		bird_x -= 2;
+		gotoxy(bird_y, bird_x);
+		putchar('@');
 		return true;
 	}
 	return false;
 }
 //解释：与输入值有关的更新
 bool Isdie() {
-	if (bird_x >= Map_height || Map[bird_x][bird_y] == 2)
+	if (bird_x >= bird_Map_height || bird_Map[bird_x][bird_y] == 2)
 		return true;
 	else return false;
 }
 //解释：判断鸟的死亡条件，也是二维数组的坐标关系
 void Update1() {
-	bird_x++;//小鸟下落一个格子
-	tube_x--;//水管向前一个格子
 	Sleep(150);
+	gotoxy(bird_y, bird_x);
+	putchar(' ');
+	bird_Map[bird_x][bird_y] = 0;
+	bird_x++;//小鸟下落一个格子
+	gotoxy(bird_y, bird_x);
+	putchar('@');
+	bird_Map[bird_x][bird_y] = 1;
+
+	if (tube_x > 1) {
+		for (int j = 1; j < tube_y; j++) {
+			gotoxy(tube_x, j);
+			putchar(' ');
+			bird_Map[j][tube_x] = 0;
+		}
+		for (int j = tube_y + 4; j < bird_Map_height; j++) {
+			gotoxy(tube_x, j);
+			putchar(' ');
+			bird_Map[j][tube_x] = 0;
+		}
+	}
+	tube_x--;//水管向前一个格子
+	if (tube_x > 1){
+		for (int j = 1; j < tube_y; j++) {
+			gotoxy(tube_x, j);
+			putchar('+');
+			bird_Map[j][tube_x] = 2;
+		}
+		for (int j = tube_y + 4; j < bird_Map_height; j++) {
+			gotoxy(tube_x, j);
+			putchar('+');
+			bird_Map[j][tube_x] = 2;
+		}
+	}
 }
 //解释：与输入无关的更新，就是无论如何都会进行的更新
 void GameRunner() {
+	InitMap();
+	//解释：每次根据更新后数组上值的变化来更新地图
+	draw();
 	while (1) {
-		InitMap();
-		//解释：每次根据更新后数组上值的变化来更新地图
-		draw();
 		Update1();
 		if (Isdie()) {
 			return;
 		}
 		else {
 			if (Update()) {
-				bird_x--;
 				if (bird_x < 2) bird_x = 1;
-				Map[bird_x][bird_y] = 1;
-
+				bird_Map[bird_x][bird_y] = 1;
 			}
 			if (bird_y == tube_x + 1) {
-				Score++;
+				bird_Score++;
+				gotoxy(15, 22);
+				printf("%d",bird_Score);
 			}
 			if (tube_x < 0) {
-				srand(time(NULL));
-				tube_x = 3 * Map_width / 4;
-				do {
-					tube_y = rand() % 20;
-				} while (tube_y <= 4 || tube_y >= 16);
+				srand((unsigned int)time(NULL));
+				tube_y = rand() % 11 + 5;
+				tube_x = 4 * bird_Map_width / 5;
 			}
 		}
 	}
@@ -134,11 +167,10 @@ void bird() {
 		InputBox((LPTSTR)buf, 20, TEXT("还玩吗？(Y/N)"));
 		//解释：交互窗口，问用户还要不要继续玩，第一个参数是缓冲区的数组（根据定义强制类型转换），第二个参数是输入字符限制，第三个是窗口上的文字
 		if (buf[0] != 'Y') {
-
 			enDgame();
 			break;
 		}
-		Score = 0;
+		bird_Score = 0;
 	}
 
 }
--- Snake.cpp
+++ Snake.cpp
@@ -11,7 +11,7 @@ int xy[2];//起始位置
 int bk[16][16];//地图大小
 int pp, qq;//果实位置
 int snakeBody[800], front, rear;
-int flag;
+int snake_flag;
 
 bool enqueue(int t) {
 	if (front == (rear + 1) % MaxSnakeLength)return 0;
@@ -40,19 +40,20 @@ void Snake_InitMap() {
 	for (int i = 1; i <= Snake_Map_Height; i++) {
 		bk[15][i] = 1;
 	}
-	srand(time(NULL));
+	srand((unsigned int)time(NULL));
 	do  {
 		pp = rand() % 15 + 1;
 		qq = rand() % 15 + 1;
 	} while (bk[pp][qq] != 0);
 	bk[pp][qq] = 2;
-	look[0] = rand() % 4 + 1;
+	look[0] = rand() % 4 + 1;//起始朝向
 	look[1] = 1;//蛇身长度
 	look[2] = look[3] = 0;
 	xy[0] = xy[1] = 7;//起始位置
 	front = rear = 0;
 	enqueue(xy[1]);
 	enqueue(xy[0]);
+	HideCursor();
 }
 
 void snake_draw() {
@@ -76,16 +77,16 @@ void snake_draw() {
 		}
 		printf("\n");
 	}
-	//gotoxy(60, 0);
+	system("color 70");
 	//BackGround(7, 0); //背景颜色的代码： 0=黑色  1蓝色 2 绿色 3湖蓝色 4红色 5紫色 6黄色 7白色 8灰色 9淡蓝色
-	printf("现在得分是:%d,请再接再厉!^_^", look[2]);
-	Sleep(150);
+	printf("现在得分是:%d     请再接再厉!^_^", look[2]);
 }
 
 void snake() {
 	Snake_InitMap();
+	snake_draw();
 	while (1) {
-		snake_draw();
+		Sleep(150);
 //---------------------读取键盘并移动------------------------------//
 		if (_kbhit()) {//记录按下的是哪个方向键
 			b[0] = _getch();//用b来记录
@@ -123,7 +124,7 @@ void snake() {
 		if (bk[xy[1]][xy[0]] == 2) {
 			look[2]++;//分数加一
 			look[1]++;//长度加一
-			flag = 1;
+			snake_flag = 1;
 			
 			//更新分数
 			while (bk[pp][qq] != 0) {
@@ -131,33 +132,33 @@ void snake() {
 				qq = rand() % 15 + 1;
 			}
 			bk[pp][qq] = 2;//将这个地方变为果实
-			gotoxy(pp * 2, qq);
-			//BackGround(0, 2);
-			printf("  ");
+			gotoxy(qq, pp);
+			putchar('@');
 		}
 
 //--------------------如果撞墙或者吃到自己----------------------//
 			//如果撞了墙或者自己//
 		if (bk[xy[1]][xy[0]] == 1 || bk[xy[1]][xy[0]] == 3) {
 			look[3] = 1;//表示已经输了
-			gotoxy(6, 6);
-			//BackGround(7, 0);
 			system("cls");
 			break;
 		}
 //--------------------刷新蛇的位置-----------------------------//
-		//gotoxy(i * 2, j);
-		//BackGround(0, 0);
-		//printf("  ");
 		bk[xy[1]][xy[0]] = 3;//使这个位置变成蛇
+		gotoxy(xy[0],xy[1]);
+		putchar('0');
 		enqueue(xy[1]); enqueue(xy[0]);
-		if (flag == 0) {
+		if (snake_flag == 0) {
 			dequeue(&temp_y); dequeue(&temp_x);
 			bk[temp_y][temp_x] = 0;
+			gotoxy(temp_x, temp_y);
+			putchar(' ');
 		}
 		else
-			flag = 0;
+			snake_flag = 0;
+		gotoxy(12, 16);
+		printf("%d",look[2]);
 	}
-	printf("你输了，最后得分:%d", look[2]);
 	enDgame();
+	system("color 07");
 }
